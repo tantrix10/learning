35041ee9f134a6be0bb2814703148c64caadf01c
Jamie Friel <jamiefriel@googlemail.com>
2020-12-23 22:31:45 +0000

    variables channenge and folder rename

---
+++ code/rust/educative/rust_from_scratch/(2) variables/defining_variables.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 1000;
+    let y = "Programming";
+    print!("x: {}\n",x);
+    print!("y: {}\n",y);
+
+    x = 1100;
+
+    print!("x: {}\n",x);
+    print!("y: {}\n",y);
+}
